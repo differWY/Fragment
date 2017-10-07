183fbb70387bfae90914ba8e9d99076bf09bb879
differ_wy <differ_wy>
2017-10-07 12:57:53 +0800

    add some example2015 to fragment repository

---
+++ mini_example/幸运抽奖（2015）/js/luckdraw.js
@@ -0,0 +1,62 @@
+﻿
+
+
+/*页面加载*/
+window.onload = function(){
+	//获取到改变区域
+	var changeArea = document.getElementById("changeArea");
+
+	//获取开始和停止按钮
+	var start = document.getElementById("start");
+	var stop = document.getElementById("stop");
+
+	//声明奖品信息数组
+	var prizeList = ['百度网盘1G空间','iphone6 PLUS','精美HelloKitty抱枕','谢谢惠顾','积分500',
+		'美的变频空调','华为P8','清风柔顺面纸','360安全公仔'];
+
+	//声明定时器变量 和键盘抬起的判定变量
+	var timer = "";
+	var keyupflag = 0;
+
+	/*开始抽奖函数*/
+	function startDraw(){
+		clearInterval(timer);
+		//使用声明的定时器变量timer 设定定时器
+		timer = setInterval(function(){
+			var randomIndex = Math.floor(Math.random()*prizeList.length);
+			changeArea.innerHTML = prizeList[randomIndex];
+		},100)
+		start.style.backgroundColor="#ccc";
+		//开始抽奖时，将键盘判断置为1
+		keyupflag = 1;
+	}
+	/*停止抽奖函数*/
+	function stopDraw(){
+		clearInterval(timer);
+		start.style.backgroundColor="#c65";
+		//停止抽奖时，将键盘判断置为0
+		keyupflag = 0;
+	}
+
+	/*键盘抬起控制抽奖的函数*/
+	function keyupDraw(keycode){
+		document.onkeyup = function(event){
+			event = event || window.event;
+			//获取键盘按下的时候键盘的键码值
+			console.log(event.keyCode);	
+			if(event.keyCode == keycode){
+				//使用到声明的键盘抬起判定变量keyupflag，判定执行细节
+				if(keyupflag == 0){
+					startDraw();
+				}else{
+					stopDraw();
+				}
+			}
+		}
+	}
+
+	//相应的事件调用函数
+	keyupDraw(32);
+	start.onclick = startDraw;
+	stop.onclick = stopDraw;
+}
---
+++ mini_example/选项卡（2015）/js/tab.js
@@ -0,0 +1,143 @@
+﻿
+//页面加载
+window.onload = function(){
+
+	//通过class获取节点的函数 getByClass(className)
+	function getByClass(className){
+		var objs = document.getElementsByTagName('*');
+		var arr = [];
+		var reg =new RegExp("\\b" + className + "\\b","g");
+		for(var i=0;i<objs.length;i++){
+			if(objs[i].className.search(reg) != -1){//search方法第一次接触
+				arr.push(objs[i]);
+			}
+		}
+		return arr;
+	}
+
+	//获取指定类名classEle下第(classIndex+1)个div里边ul里边的li数组
+	function getLis(classEle,classIndex){
+		var childs = classEle[classIndex].childNodes;
+		var cChilds = "";
+		var lis = [];
+		for(var i=0;i<childs.length;i++){
+			if(childs[i].tagName == 'UL'){
+				cChilds = childs[i].childNodes;
+			}
+		}
+		for(var i=0;i<cChilds.length;i++){
+			if(cChilds[i].tagName == 'LI'){
+				lis.push(cChilds[i]);
+			}
+		}
+		return lis;
+	}//这个函数写的没有任何复用性，而且可以用getElementsByTagName('li');代替
+	//实际上可以直接用getElementsByTagName('li');代替
+
+	//获取某个类指定下标节点元素内的指定标签名的子节点数组
+	function getChlidsByTagName(classLable,classIndex,tag){
+		var aResult = [];
+		var childs = classLable[classIndex].childNodes;
+		for (var i = 0; i < childs.length; i++) {
+			if(childs[i].tagName == tag){
+				aResult.push(childs[i]);
+			}
+		};
+		return aResult;
+	}//实际上可以直接用getElementsByTagName('ul');代替
+
+	//获取兄弟元素(父节点的字节点，同时不是它本身),指定标签名的，
+	//传参数需大写 例：'LI'
+	function getBrothers(eleName,tag){
+		var borders = [];
+		var allChilds = eleName.parentNode.childNodes;
+		for(var i=0;i<allChilds.length;i++){
+			if(allChilds[i].tagName == tag && allChilds[i] != eleName){
+				borders.push(allChilds[i]);
+			}
+		}
+		return borders;
+	}
+
+	//给数组内的dom元素换class
+	function changeClass(arr,classLable){
+		for (var i = 0; i < arr.length; i++) {
+			arr[i].className = classLable;
+		};
+	}
+
+	//获取到节点
+	var nav = getByClass('nav');
+	var lis = getLis(nav,0);
+	var content = getByClass('content');
+	var cons = getChlidsByTagName(content,0,'UL');
+	
+	/*
+	for(var i=0;i<lis.length;i++){
+		lis[i].onclick = function(){
+			this.className = 'nav_li nav_li_on';
+			var b = getBrothers(this,'LI');
+			changeClass(b,'nav_li');
+		}
+		lis[i].onmouseover = function(){
+			this.className = 'nav_li nav_li_on';
+			var b = getBrothers(this,'LI');
+			changeClass(b,'nav_li');
+		}	
+	}
+	for(var i=0;i<lis.length;i++){
+		lis[i].index = i;//index换成id的话ie9以下就可以识别了
+		lis[i].onclick = function(){
+			for(var j=0;j<lis.length;j++){
+				lis[this.index].className = 'nav_li nav_li_on';
+				cons[this.index].className = 'con show';
+				if(this.index != j){
+					lis[j].className = 'nav_li';
+					cons[j].className = 'con hide';
+				}
+			}
+		}
+		lis[i].onmouseover = function(){
+			for(var j=0;j<lis.length;j++){
+				lis[this.index].className = 'nav_li nav_li_on';
+				cons[this.index].className = 'con show';
+				if(this.index != j){
+					lis[j].className = 'nav_li';
+					cons[j].className = 'con hide';
+				}
+			}
+		}		
+	}
+	*/
+
+	//dom2级绑定，为多个事件绑定一个函数
+	function addEvent(dom,eveName,handle,boo){
+		var arr = eveName.split(',');
+		//这里刚开始用的正则没起作用 后来发现用split就可以了，
+		//不过不能兼容空格 ，可以再优化一下
+		//思考：怎样绑定多个函数呢？
+		for(var i=0;i<arr.length;i++){
+			dom.addEventListener
+			?dom.addEventListener(arr[i],handle,boo)
+			:dom.attachEvent('on'+arr[i],handle);
+			//这里的兼容同样是针对于IE 6、7、8
+		}
+	}
+	for(var i=0;i<lis.length;i++){
+		lis[i].id = i;
+		addEvent(lis[i],'click,mouseout',function(){
+			for(var j=0;j<lis.length;j++){
+				lis[this.id].className = 'nav_li nav_li_on';
+				cons[this.id].className = 'con show';
+				if(this.id != j){
+					lis[j].className = 'nav_li';
+					cons[j].className = 'con hide';
+				}
+			}
+		},false);
+		//这样每次dom元素都需要用参数的形式 仅仅是自定义函数，
+		//需要了解js扩展，达到对象可以调用函数。
+	}
+
+
+}
